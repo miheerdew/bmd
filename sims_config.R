4d6bda6ae01a342b4b3b28c1c45c5e61f7a6a0c1
Miheer Dewaskar <miheerdew@gmail.com>
2017-01-22 18:35:31 -0500

    Forgot base_rho; Added comments

--- sims_config.R
+++ sims_config.R
@@ -1,7 +1,11 @@
-rho_knobs <- c(0,1) #Multiplier for the intra-block correlations
-saveDir <- "sims"
-doBRIM <- FALSE #Should we test BRIM
-ns <- c(100,500) #The sample sizes to run.
 bX <- 50 #Size of each X block
 bY <- 25 #Size of each Y block
-beta <- 1 #Base signal strength
+
+base_rho <- seq(0.9,0,-0.1) #The intra block correlations in X.
+rho_knobs <- c(0,1) #Multiplier to base_rho for the intra-block correlations
+
+beta <- 1 #In each Y block, Y = beta * (average value of X's in block) + noise
+
+ns <- c(100,500) #The sample sizes to run.
+saveDir <- "sims"
+doBRIM <- FALSE #Should we test BRIM?
