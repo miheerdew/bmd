f0f3d4446c42e9e557566a1ba346c1d8e4eaf08b
Miheer Dewaskar <miheerdew@gmail.com>
2017-02-10 12:13:19 -0500

    introduced random neighbors

--- run_sims.sh
+++ run_sims.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -r sims/plots/*
 Rscript sims.R
 Rscript sims_run_methods.R
 Rscript simplots.R
--- sims_config.R
+++ sims_config.R
@@ -1,10 +1,19 @@
-bX <- 50 #Size of each X block
-bY <- 25 #Size of each Y block
+#Block sizes
+bX <- 5
+bY <- 5
 
-base_rho <- seq(0.9,0,-0.1) #The intra block correlations in X.
+#Number of blocks
+nX <- 20
+nY <- 20
+
+base_rho <- rep(0.9, nX) #The intra block correlations in X.
 rho_knobs <- c(0,1) #Multiplier to base_rho for the intra-block correlations
 
 beta <- 1 #In each Y block, Y = beta * (average value of X's in block) + noise
+
+lambda <- 3
+p <- min(lambda/nX,1) #The probability of an edge
+
 
 ns <- c(100,500) #The sample sizes to run.
 saveDir <- "sims"
