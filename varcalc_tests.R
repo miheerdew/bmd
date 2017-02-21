d40658828bb458d2ce5df1238208289615ef5704
jpalowitch <johnpalowitch@gmail.com>
2017-02-21 17:24:47 -0500

    Timing and plotting for varcalc tests

--- varcalc_tests.R
+++ varcalc_tests.R
@@ -1,5 +1,5 @@
 library(MASS)
-
+library(microbenchmark)
 source("varcalcs.R")
 source("moment_calcs.R")
 
@@ -12,7 +12,7 @@ s2 <- 1
 
 SigmaX_hom <- diag(1 - rho, m) + matrix(rep(rho, m^2), ncol = m)
 
-corsums <- vars <- rep(0, nsims)
+corsums <- vars <- timers <- rep(0, nsims)
 
 for (sim in 1:nsims) {
   
@@ -21,6 +21,30 @@ for (sim in 1:nsims) {
   Data <- mvrnorm(ndata, rep(0, m), SigmaX_hom)
   Y <- Beta * rowSums(Data) + rnorm(ndata, sd = sqrt(s2))
   corsums[sim] <- sum(cor(Y, Data))
+  
+  timer <- get_nanotime()
   vars[sim] <- varcalc1(Data, as.vector(Y))
+  timers[sim] <- get_nanotime() - timer
   
 }
+
+if (!dir.exists('var_plots'))
+  dir.create('var_plots')
+
+png(file.path('var_plots', 'var_check_hom.png'))
+hist(vars, main = '')
+abline(v = popvar(rho), col = 'red', lwd = 2)
+abline(v = mean(vars), col = 'green', lwd = 2, lty = 2)
+dev.off()
+
+
+png(file.path('var_plots', 'z_check_hom.png'))
+qqnorm(sqrt(ndata) * (corsums - m * pxy(rho))/sqrt(vars))
+abline(0, 1, col = 'red')
+dev.off()
+
+png(file.path('var_plots', 'timer_hom.png'))
+hist(timers / 1e9, main = '')
+dev.off()
+
+fivenum(timers / 1e9)
