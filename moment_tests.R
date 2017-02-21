e6d3b50f30b6f207ea23f0d9199caa36feda10d9
jpalowitch <johnpalowitch@gmail.com>
2017-02-21 14:00:53 -0500

    fixed some bugs and formulas

--- moment_tests.R
+++ moment_tests.R
@@ -3,8 +3,9 @@ bX <- 20
 rhos <- 0.4
 Beta <- 1
 s2 <- 1
+nsims0 <- 1000
 nsims <- 100
-ndata <- 100000
+ndata <- 10000
 
 library(MASS)
 library(Matrix)
@@ -49,71 +50,104 @@ pyjjj <- function (r) {pbr(r) * 3 / sqrt(varY(r))}
 
 # Variance calculations
 popvar1234 <- function (r) {mu1234(r) + 0.25 * r^2 * 
-    (mu1111(r) + 3 * mu1122(r)) - rho * 2 * mu1123(r)}
+    (mu1111(r) + 3 * mu1122(r)) - r * 2 * mu1123(r)}
 
 popvar1213 <- function (r) {mu1123(r) + 0.25 * r^2 * 
-    (mu1111(r) + 3 * mu1122(r)) - rho * (mu1112(r) + mu1123(r))}
+    (mu1111(r) + 3 * mu1122(r)) - r * (mu1112(r) + mu1123(r))}
 
 popvar <- function (r) {(pyyjj(r) + 0.25 * pxy(r)^2 * (pyyyy(r) + 2 * pyyjj(r) +
     mu1122(r)) - pxy(r) * (pyyyj(r) + pyjjj(r)) + 
     (bX - 1) * (pyyjk(r) + 0.25 * pxy(r)^2 * (pyyyy(r) + 2 * pyyjj(r) +
     mu1122(r)) - pxy(r) * (pyyyj(r) + pyjjk(r)))) * bX}
 
-moments <- matrix(0, nsims, 5)
-names(moments) <- c("mu1111", "mu1112", "mu1123", "mu1122", "mu1234")
-moments <- as.data.frame(moments)
-varYs <- rep(0, nsims)
+moments0 <- matrix(0, nsims0, 5)
+moments0 <- as.data.frame(moments0)
+colnames(moments0) <- c("mu1111", "mu1112", "mu1123", "mu1122", "mu1234")
+varYs0 <- rep(0, nsims)
 
-crosscors <- matrix(0, nsims, 4)
-crosscors <- as.data.frame(crosscors)
-colnames(crosscors) <- c("ryy12", "ryy11", "ryyyy", "all4s")
+crosscors0 <- matrix(0, nsims0, 4)
+crosscors0 <- as.data.frame(crosscors0)
+colnames(crosscors0) <- c("ryy12", "ryy11", "ryyyy", "all4s")
 
-truerhos <- matrix(0, nsims, 7)
-truerhos <- as.data.frame(truerhos)
-colnames(truerhos) <- c("cross", "pyyjk", "pyyyy", "pyyjj", "pyyyj", "pyjjk",
-                        "pyjjj")
+truerhos0 <- matrix(0, nsims0, 7)
+truerhos0 <- as.data.frame(truerhos0)
+colnames(truerhos0) <- c("cross", "pyyjk", "pyyyy", "pyyjj", "pyyyj", "pyjjk",
+                         "pyjjj")
 
-covars <- matrix(0, nsims, 4)
-covars <- as.data.frame(covars)
-colnames(covars) <- c("ysum", "r12", "r13", "r34")
+crosscovars <- matrix(0, nsims0, 3)
+crosscovars <- as.data.frame(crosscovars)
+colnames(crosscovars) <- c("ysum", "r1213", "r1234")
 
-for (sim in 1:nsims) {
+for (sim0 in 1:nsims0) {
   
-  cat("sim", sim, "\n")
+  moments <- matrix(0, nsims, 5)
+  moments <- as.data.frame(moments)
+  colnames(moments) <- c("mu1111", "mu1112", "mu1123", "mu1122", "mu1234")
+  varYs <- rep(0, nsims)
   
-  Data <- mvrnorm(ndata, rep(0, bX), SigmaX)
-  Y <- Data %*% rep(Beta, bX) + rnorm(ndata, sd = sqrt(s2))
-  moments[sim, 'mu1111'] <- mean(Data[ , 1]^4)
-  moments[sim, 'mu1112'] <- mean(Data[ , 1]^3 * Data[ , 2])
-  moments[sim, 'mu1123'] <- mean(Data[ , 1]^2 * Data[ , 2] * Data[ , 3])
-  moments[sim, 'mu1122'] <- mean(Data[ , 1]^2 * Data[ , 2]^2)
-  moments[sim, 'mu1234'] <- mean(Data[ , 1] * Data[ , 2] * 
-                                   Data[ , 3] * Data[ , 4])
-  varYs[sim] <- var(Y)
+  crosscors <- matrix(0, nsims, 4)
+  crosscors <- as.data.frame(crosscors)
+  colnames(crosscors) <- c("ryy12", "ryy11", "ryyyy", "all4s")
   
-  crosscors[sim, 'ryy12'] <- mean(Y^2 * Data[ , 1] * Data[ , 2]) /
-    (var(Y) * sd(Data[ , 1] * sd(Data[ , 2])))
-  crosscors[sim, 'ryy11'] <- mean(Y^2 * Data[ , 1]^2) / 
-    (var(Y) * var(Data[ , 1]))
-  crosscors[sim, 'ryyyy'] <- mean(Y^4) / sd(Y)^4
-  crosscors[sim, 'all4s'] <- mean((Data %*% rep(1, bX))^4)
+  truerhos <- matrix(0, nsims, 7)
+  truerhos <- as.data.frame(truerhos)
+  colnames(truerhos) <- c("cross", "pyyjk", "pyyyy", "pyyjj", "pyyyj", "pyjjk",
+                          "pyjjj")
   
-  sdY <- sd(Y); sdj <- sd(Data[ , 1]); sdk <- sd(Data[ , 2])
-  truerhos[sim, 'cross'] <- mean(Y * Data[ , 1])
-  truerhos[sim, 'pyyjk'] <- mean(Y^2 * Data[ , 1] * Data[ , 2]) / 
-    (sdY^2 * sdj * sdk)
-  truerhos[sim, 'pyyyy'] <- mean(Y^4) / (sdY^4)
-  truerhos[sim, 'pyyjj'] <- mean(Y^2 * Data[ , 1]^2) / (sdY^2 * sdj^2)
-  truerhos[sim, 'pyyyj'] <- mean(Y^3 * Data[ , 1]) / (sdY^3 * sdj)
-  truerhos[sim, 'pyjjk'] <- mean(Y * Data[ , 1]^2 * Data[ , 2]) / 
-    (sdY * sdj^2 * sdk)
-  truerhos[sim, 'pyjjj'] <- mean(Y * Data[ , 1]^3 / (sdY * sdj^3))
+  covars <- matrix(0, nsims, 4)
+  covars <- as.data.frame(covars)
+  colnames(covars) <- c("ysum", "r12", "r13", "r34")
   
-  covars[sim, "ysum"] <- sum(cor(Y, Data))
-  covars[sim, "r12"] <- cor(Data[ , 1], Data[ , 2])
-  covars[sim, "r13"] <- cor(Data[ , 1], Data[ , 3])
-  covars[sim, "r34"] <- cor(Data[ , 3], Data[ , 4])
+  for (sim in 1:nsims) {
+    
+    simpos <- (sim0 - 1) * nsims + sim
+    
+    cat("simpos", simpos, "\n")
+    
+    Data <- mvrnorm(ndata, rep(0, bX), SigmaX)
+    Y <- Data %*% rep(Beta, bX) + rnorm(ndata, sd = sqrt(s2))
+    moments[sim, 'mu1111'] <- mean(Data[ , 1]^4)
+    moments[sim, 'mu1112'] <- mean(Data[ , 1]^3 * Data[ , 2])
+    moments[sim, 'mu1123'] <- mean(Data[ , 1]^2 * Data[ , 2] * Data[ , 3])
+    moments[sim, 'mu1122'] <- mean(Data[ , 1]^2 * Data[ , 2]^2)
+    moments[sim, 'mu1234'] <- mean(Data[ , 1] * Data[ , 2] * 
+                                     Data[ , 3] * Data[ , 4])
+    varYs[sim] <- var(Y)
+    
+    crosscors[sim, 'ryy12'] <- mean(Y^2 * Data[ , 1] * Data[ , 2]) /
+      (var(Y) * sd(Data[ , 1] * sd(Data[ , 2])))
+    crosscors[sim, 'ryy11'] <- mean(Y^2 * Data[ , 1]^2) / 
+      (var(Y) * var(Data[ , 1]))
+    crosscors[sim, 'ryyyy'] <- mean(Y^4) / sd(Y)^4
+    crosscors[sim, 'all4s'] <- mean((Data %*% rep(1, bX))^4)
+    
+    sdY <- sd(Y); sdj <- sd(Data[ , 1]); sdk <- sd(Data[ , 2])
+    truerhos[sim, 'cross'] <- mean(Y * Data[ , 1])
+    truerhos[sim, 'pyyjk'] <- mean(Y^2 * Data[ , 1] * Data[ , 2]) / 
+      (sdY^2 * sdj * sdk)
+    truerhos[sim, 'pyyyy'] <- mean(Y^4) / (sdY^4)
+    truerhos[sim, 'pyyjj'] <- mean(Y^2 * Data[ , 1]^2) / (sdY^2 * sdj^2)
+    truerhos[sim, 'pyyyj'] <- mean(Y^3 * Data[ , 1]) / (sdY^3 * sdj)
+    truerhos[sim, 'pyjjk'] <- mean(Y * Data[ , 1]^2 * Data[ , 2]) / 
+      (sdY * sdj^2 * sdk)
+    truerhos[sim, 'pyjjj'] <- mean(Y * Data[ , 1]^3 / (sdY * sdj^3))
+    
+    covars[sim, 'ysum'] <- sum(cor(Y, Data))
+    covars[sim, 'r12'] <- cor(Data[ , 1], Data[ , 2])
+    covars[sim, 'r13'] <- cor(Data[ , 1], Data[ , 3])
+    covars[sim, 'r34'] <- cor(Data[ , 3], Data[ , 4])
+    
+  }
   
+  moments0[sim0, ] <- colMeans(moments)
+  crosscors0[sim0, ] <- colMeans(crosscors)
+  truerhos0[sim0, ] <- colMeans(truerhos)
+  varYs0[sim0] <- mean(varYs)
+  
+  crosscovars[sim0, 'ysum'] <- var(covars$ysum)
+  crosscovars[sim0, 'r1213'] <- cov(covars$r12, covars$r13)
+  crosscovars[sim0, 'r1234'] <- cov(covars$r12, covars$r34)
+    
 }
 
 if (!dir.exists('moment_plots'))
@@ -122,86 +156,101 @@ if (!dir.exists('moment_plots'))
 png(file.path('moment_plots', 'moment_plot1.png'))
 par(mfrow = c(2, 3))
 
-hist(moments$mu1111, main = '')
+hist(moments0$mu1111, main = '')
 abline(v = mu1111(rhos), col = 'red', lwd = 2)
-abline(v = mean(moments$mu1111), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(moments0$mu1111), col = 'green', lty = 2, lwd = 2)
 
-hist(moments$mu1112, main = '')
+hist(moments0$mu1112, main = '')
 abline(v = mu1112(rhos), col = 'red', lwd = 2)
-abline(v = mean(moments$mu1112), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(moments0$mu1112), col = 'green', lty = 2, lwd = 2)
 
-hist(moments$mu1122, main = '')
+hist(moments0$mu1122, main = '')
 abline(v = mu1122(rhos), col = 'red', lwd = 2)
-abline(v = mean(moments$mu1122), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(moments0$mu1122), col = 'green', lty = 2, lwd = 2)
 
-hist(moments$mu1123, main = '')
+hist(moments0$mu1123, main = '')
 abline(v = mu1123(rhos), col = 'red', lwd = 2)
-abline(v = mean(moments$mu1123), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(moments0$mu1123), col = 'green', lty = 2, lwd = 2)
 
-hist(moments$mu1234, main = '')
+hist(moments0$mu1234, main = '')
 abline(v = mu1234(rhos), col = 'red', lwd = 2)
-abline(v = mean(moments$mu1234), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(moments0$mu1234), col = 'green', lty = 2, lwd = 2)
 
-hist(varYs, main = '')
+hist(varYs0, main = '')
 abline(v = varY(rhos), col = 'red', lwd = 2)
-abline(v = mean(varYs), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(varYs0), col = 'green', lty = 2, lwd = 2)
 
 dev.off()
 
 png(file.path('moment_plots', 'moment_plot2.png'))
 par(mfrow = c(2, 3))
 
-hist(crosscors$ryy12)
+hist(crosscors0$ryy12, main = '')
 abline(v = pyy12(rhos), col = 'red', lwd = 2)
-abline(v = mean(crosscors$ryy12), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(crosscors0$ryy12), col = 'green', lty = 2, lwd = 2)
 
-hist(crosscors$ryy11)
+hist(crosscors0$ryy11, main = '')
 abline(v = pyy11(rhos), col = 'red', lwd = 2)
-abline(v = mean(crosscors$ryy11), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(crosscors0$ryy11), col = 'green', lty = 2, lwd = 2)
 
-hist(crosscors$ryyyy)
+hist(crosscors0$ryyyy, main = '')
 abline(v = pyyyy(rhos), col = 'red', lwd = 2)
-abline(v = mean(crosscors$ryyyy), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(crosscors0$ryyyy), col = 'green', lty = 2, lwd = 2)
 
-hist(crosscors$all4s)
+hist(crosscors0$all4s, main = '')
 abline(v = all4s(rhos), col = 'red', lwd = 2)
-abline(v = mean(crosscors$all4s), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(crosscors0$all4s), col = 'green', lty = 2, lwd = 2)
 
 dev.off()
 
 png(file.path('moment_plots', 'moment_plot3.png'))
 par(mfrow = c(3, 3))
 
-hist(truerhos$cross)
+hist(truerhos0$cross, main = '')
 abline(v = pbr(rhos), col = 'red', lwd = 2)
-abline(v = mean(truerhos$cros), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(truerhos0$cros), col = 'green', lty = 2, lwd = 2)
 
-hist(truerhos$pyyjk)
+hist(truerhos0$pyyjk, main = '')
 abline(v = pyyjk(rhos), col = 'red', lwd = 2)
-abline(v = mean(truerhos$pyyjk), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(truerhos0$pyyjk), col = 'green', lty = 2, lwd = 2)
 
-hist(truerhos$pyyyy)
+hist(truerhos0$pyyyy, main = '')
 abline(v = pyyyy(rhos), col = 'red', lwd = 2)
-abline(v = mean(truerhos$pyyyy), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(truerhos0$pyyyy), col = 'green', lty = 2, lwd = 2)
 
-hist(truerhos$pyyjj)
+hist(truerhos0$pyyjj, main = '')
 abline(v = pyyjj(rhos), col = 'red', lwd = 2)
-abline(v = mean(truerhos$pyyjj), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(truerhos0$pyyjj), col = 'green', lty = 2, lwd = 2)
 
-hist(truerhos$pyyyj)
+hist(truerhos0$pyyyj, main = '')
 abline(v = pyyyj(rhos), col = 'red', lwd = 2)
-abline(v = mean(truerhos$pyyyj), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(truerhos0$pyyyj), col = 'green', lty = 2, lwd = 2)
 
-hist(truerhos$pyjjk)
+hist(truerhos0$pyjjk, main = '')
 abline(v = pyjjk(rhos), col = 'red', lwd = 2)
-abline(v = mean(truerhos$pyjjk), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(truerhos0$pyjjk), col = 'green', lty = 2, lwd = 2)
 
-hist(truerhos$pyjjj)
+hist(truerhos0$pyjjj, main = '')
 abline(v = pyjjj(rhos), col = 'red', lwd = 2)
-abline(v = mean(truerhos$pyjjj), col = 'green', lty = 2, lwd = 2)
+abline(v = mean(truerhos0$pyjjj), col = 'green', lty = 2, lwd = 2)
 
 dev.off()
 
+
 png(file.path('moment_plots', 'covars.png'))
 par(mfrow = c(1, 3))
+
+hist(crosscovars$ysum * ndata, main = '')
+abline(v = popvar(rhos), col = 'red', lwd = 2)
+abline(v = mean(crosscovars$ysum) * ndata, col = 'green', lty = 2, lwd = 2)
+
+hist(crosscovars$r1234 * ndata, main = '')
+abline(v = popvar1234(rhos), col = 'red', lwd = 2)
+abline(v = mean(crosscovars$r1234) * ndata, col = 'green', lty = 2, lwd = 2)
+
+hist(crosscovars$r1213 * ndata, main = '')
+abline(v = popvar1213(rhos), col = 'red', lwd = 2)
+abline(v = mean(crosscovars$r1213) * ndata, col = 'green', lty = 2, lwd = 2)
+
+dev.off()
 
