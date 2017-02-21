b96516441c93570f686d33cb3f4a60d876092a81
jpalowitch <johnpalowitch@gmail.com>
2017-02-21 12:36:57 -0500

    Finalized overall covar checking.

--- moment_tests.R
+++ moment_tests.R
@@ -20,6 +20,9 @@ SigmaX <- as.matrix(bdiag(rho_blocksX))
 # Useful functions
 d <- function (r) {1 - 3 * r^2 + 2 * r^3}
 pbr <- function (r) {Beta * (1 - r + bX * r)}
+pxy <- function (r) {pbr(r) / sqrt(varY(r))}
+
+# Function for single variance calculation
 varY <- function (r) {bX * Beta * (1 - r + r * bX) + s2}
 mu1111 <- function (r) {3}
 mu1112 <- function (r) {3 * r}
@@ -27,6 +30,7 @@ mu1123 <- function (r) {2 * r^2 + r}
 mu1122 <- function (r) {1 + 2 * r^2}
 mu1234 <- function (r) {3 * r * (r - 1)^2 * (2 * r^2 + r) / d(r)}
 
+# Functions for total variance calculation
 pyy12  <- function (r) {((bX - 2) * mu1123(r) + (bX - 2) * (bX - 3) * mu1234(r) +
     4 * (bX - 2) * mu1123(r) + 2 * (mu1112(r) + mu1122(r)) + s2 * r) / varY(r)}
 pyy11 <- function (r) {((bX - 1) * mu1122(r) + (bX - 1) * (bX - 2) * mu1123(r) +
@@ -34,7 +38,6 @@ pyy11 <- function (r) {((bX - 1) * mu1122(r) + (bX - 1) * (bX - 2) * mu1123(r) +
 all4s <- function (r) {3 * sum(SigmaX)^2}
 pyyyy <- function (r) {(all4s(r) + 
     6 * s2 * Beta^2 * bX * (1 - r + bX * r) + s2^2 * mu1111(r)) / varY(r)^(1.5)}
-
 pyyjk <- function (r) {(2 * pbr(r)^2 * (2 * r + 1) / (r + 1) + 
                           r * Beta * pbr(r) * (bX + s2 / (Beta * pbr(r)) - 
                               2 * (1 - r + bX * r) / (1 + r))) / varY(r)}
@@ -42,8 +45,19 @@ pyyyy <- function (r) {3}
 pyyjj <- function (r) {(2 * pbr(rhos)^2 + varY(rhos)) / varY(rhos)}
 pyyyj <- function (r) {3 * varY(rhos) * pbr(rhos) / varY(rhos)^(3 / 2)}
 pyjjk <- function (r) {pbr(r) * (2 * r + 1) / sqrt(varY(r))}
+pyjjj <- function (r) {pbr(r) * 3 / sqrt(varY(r))}
 
+# Variance calculations
+popvar1234 <- function (r) {mu1234(r) + 0.25 * r^2 * 
+    (mu1111(r) + 3 * mu1122(r)) - rho * 2 * mu1123(r)}
 
+popvar1213 <- function (r) {mu1123(r) + 0.25 * r^2 * 
+    (mu1111(r) + 3 * mu1122(r)) - rho * (mu1112(r) + mu1123(r))}
+
+popvar <- function (r) {(pyyjj(r) + 0.25 * pxy(r)^2 * (pyyyy(r) + 2 * pyyjj(r) +
+    mu1122(r)) - pxy(r) * (pyyyj(r) + pyjjj(r)) + 
+    (bX - 1) * (pyyjk(r) + 0.25 * pxy(r)^2 * (pyyyy(r) + 2 * pyyjj(r) +
+    mu1122(r)) - pxy(r) * (pyyyj(r) + pyjjk(r)))) * bX}
 
 moments <- matrix(0, nsims, 5)
 names(moments) <- c("mu1111", "mu1112", "mu1123", "mu1122", "mu1234")
@@ -51,12 +65,17 @@ moments <- as.data.frame(moments)
 varYs <- rep(0, nsims)
 
 crosscors <- matrix(0, nsims, 4)
-names(crosscors) <- c("ryy12", "ryy11", "ryyyy", "all4s")
 crosscors <- as.data.frame(crosscors)
+colnames(crosscors) <- c("ryy12", "ryy11", "ryyyy", "all4s")
 
-truerhos <- matrix(0, nsims, 6)
-names(truerhos) <- c("cross", "pyyjk", "pyyyy", "pyyjj", "pyyyj", "pyjjk")
+truerhos <- matrix(0, nsims, 7)
 truerhos <- as.data.frame(truerhos)
+colnames(truerhos) <- c("cross", "pyyjk", "pyyyy", "pyyjj", "pyyyj", "pyjjk",
+                        "pyjjj")
+
+covars <- matrix(0, nsims, 4)
+covars <- as.data.frame(covars)
+colnames(covars) <- c("ysum", "r12", "r13", "r34")
 
 for (sim in 1:nsims) {
   
@@ -88,6 +107,12 @@ for (sim in 1:nsims) {
   truerhos[sim, 'pyyyj'] <- mean(Y^3 * Data[ , 1]) / (sdY^3 * sdj)
   truerhos[sim, 'pyjjk'] <- mean(Y * Data[ , 1]^2 * Data[ , 2]) / 
     (sdY * sdj^2 * sdk)
+  truerhos[sim, 'pyjjj'] <- mean(Y * Data[ , 1]^3 / (sdY * sdj^3))
+  
+  covars[sim, "ysum"] <- sum(cor(Y, Data))
+  covars[sim, "r12"] <- cor(Data[ , 1], Data[ , 2])
+  covars[sim, "r13"] <- cor(Data[ , 1], Data[ , 3])
+  covars[sim, "r34"] <- cor(Data[ , 3], Data[ , 4])
   
 }
 
@@ -145,7 +170,7 @@ abline(v = mean(crosscors$all4s), col = 'green', lty = 2, lwd = 2)
 dev.off()
 
 png(file.path('moment_plots', 'moment_plot3.png'))
-par(mfrow = c(2, 3))
+par(mfrow = c(3, 3))
 
 hist(truerhos$cross)
 abline(v = pbr(rhos), col = 'red', lwd = 2)
@@ -171,4 +196,12 @@ hist(truerhos$pyjjk)
 abline(v = pyjjk(rhos), col = 'red', lwd = 2)
 abline(v = mean(truerhos$pyjjk), col = 'green', lty = 2, lwd = 2)
 
+hist(truerhos$pyjjj)
+abline(v = pyjjj(rhos), col = 'red', lwd = 2)
+abline(v = mean(truerhos$pyjjj), col = 'green', lty = 2, lwd = 2)
+
 dev.off()
+
+png(file.path('moment_plots', 'covars.png'))
+par(mfrow = c(1, 3))
+
