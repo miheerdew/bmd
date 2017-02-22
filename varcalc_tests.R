373ce8a79a78ad882f0009a2cf1276571d0f729e
jpalowitch <johnpalowitch@gmail.com>
2017-02-22 10:45:36 -0500

    Improvements to varcals:
      * varcalc1 is simpler and faster
      * Added varcalc1_multi for multiple ys
      * added illustrative plots

--- varcalc_tests.R
+++ varcalc_tests.R
@@ -1,9 +1,10 @@
-library(MASS)
+source("mvrnormR.R")
 library(microbenchmark)
 source("varcalcs.R")
 source("moment_calcs.R")
 
 m <- 20
+my <- 10
 rho <- 0.4
 nsims <- 1000
 ndata <- 1000
@@ -12,19 +13,50 @@ s2 <- 1
 
 SigmaX_hom <- diag(1 - rho, m) + matrix(rep(rho, m^2), ncol = m)
 
-corsums <- vars <- timers <- rep(0, nsims)
+corsums <- corsums_multi <- vars <- vars_multi <- rep(0, nsims * my)
+simcorsums <- simvars <- rep(0, my)
+timers <- timers_multi <- rep(0, nsims)
+
+set.seed(12345)
 
 for (sim in 1:nsims) {
   
+  simpos <- (sim - 1) * my
+  posindx <- (simpos + 1):(simpos + m)
+  
   cat("sim ", sim, "\n")
   
-  Data <- mvrnorm(ndata, rep(0, m), SigmaX_hom)
-  Y <- Beta * rowSums(Data) + rnorm(ndata, sd = sqrt(s2))
-  corsums[sim] <- sum(cor(Y, Data))
+  Data <- mvrnormR(ndata, rep(0, m), SigmaX_hom)
+  Yvars <- Beta * rowSums(Data) + matrix(rnorm(ndata * my, sd = sqrt(s2)),
+                                         ncol = my)
+  
+  
+  # Looped calculation
+
+  timer <- get_nanotime()
+  
+  for (yindx in 1:my) {
+    simcorsums[yindx] <- sum(cor(Yvars[ , yindx], Data))
+    simvars[yindx] <- varcalc1(as.vector(Yvars[ , yindx]), Data)
+  }
+  corsums[posindx] <- simcorsums
+  vars[posindx] <- simvars
+  
+  timers[sim] <- get_nanotime() - timer
+
+  
+  # Vectorized calculation
   
   timer <- get_nanotime()
-  vars[sim] <- varcalc1(Data, as.vector(Y))
-  timers[sim] <- get_nanotime() - timer
+  
+  corsums_multi[posindx] <- as.vector(rowSums(cor(Yvars, Data)))
+  vars_multi[posindx] <- varcalc1_multi(Yvars, Data)
+  
+  timers_multi[sim] <- get_nanotime() - timer
+  
+  
+  if (sum((vars_multi[posindx] - vars[posindx])^2) > 1e-15 * my)
+    break
   
 }
 
@@ -34,17 +66,25 @@ if (!dir.exists('var_plots'))
 png(file.path('var_plots', 'var_check_hom.png'))
 hist(vars, main = '')
 abline(v = popvar(rho), col = 'red', lwd = 2)
-abline(v = mean(vars), col = 'green', lwd = 2, lty = 2)
+abline(v = mean(vars_multi), col = 'green', lwd = 2, lty = 2)
 dev.off()
 
 
 png(file.path('var_plots', 'z_check_hom.png'))
-qqnorm(sqrt(ndata) * (corsums - m * pxy(rho))/sqrt(vars))
+qqnorm(sqrt(ndata) * (corsums_multi - m * pxy(rho)) / sqrt(vars_multi))
 abline(0, 1, col = 'red')
 dev.off()
 
+tmplot <- timers_multi / 1e9
+tmplot <- tmplot[tmplot < quantile(tmplot, .99) & 
+                   tmplot > quantile(tmplot, .01)]
+
 png(file.path('var_plots', 'timer_hom.png'))
-hist(timers / 1e9, main = '')
+hist(tmplot, main = 'Timers for multiple Ys')
+dev.off()
+
+png(file.path('var_plots', 'timer_compare_hom.png'))
+hist(log2(timers / timers_multi), main = '')
 dev.off()
 
 fivenum(timers / 1e9)
--- varcalcs.R
+++ varcalcs.R
@@ -1,4 +1,4 @@
-varcalc1 <- function (Xmat, Yvec) {
+varcalc1 <- function (Yvec, Xmat) {
   
   Xmat <- scale(Xmat)
   Yvec <- as.vector(scale(Yvec))
@@ -6,34 +6,72 @@ varcalc1 <- function (Xmat, Yvec) {
   n <- nrow(Xmat)
   
   if (n != length(Yvec))
-    stop('X and Yvec variables must be of same dimension\n')
+    stop('X and Y variables must be of same dimension\n')
   
   # General calcs
   xyCors <- as.vector(cor(Yvec, Xmat))
-  xyDoubleCors <- as.vector(crossprod(Yvec^2, Xmat^2))
-  xxDoubleCors <- crossprod(Xmat^2)
   y4 <- sum(Yvec^4)
+  xRowSum <- rowSums(Xmat)
+  xRowSum2 <- tcrossprod(xyCors, Xmat^2)
   
   # Calc for star 1
-  xyMat <- Xmat * Yvec
-  star1 <- sum(crossprod(xyMat))
+  star1 <- crossprod(Yvec^2, xRowSum^2)
   
   # Calc for star 2
   star2 <- y4 * sum(xyCors)^2
   
   # Calc for star 3
-  star3 <- 2 * sum(xyCors) * sum(xyCors * xyDoubleCors)
+  star3 <- 2 * sum(xyCors) * crossprod(Yvec^2, t(xRowSum2))
   
   # Calc for star 4
-  star4 <- t(xyCors) %*% xxDoubleCors %*% xyCors
+  star4 <- tcrossprod(xRowSum2)
   
   # Calc for dagger 1
-  y3x <- as.vector(crossprod(Yvec^3, Xmat))
-  dagger1 <- sum(xyCors) * sum(y3x)
+  dagger1 <- sum(xyCors) * crossprod(Yvec^3, xRowSum)
   
   # Calc for dagger 2
-  yxjjkmat <- crossprod(Xmat * Yvec, Xmat^2)
-  dagger2 <- sum(yxjjkmat %*% xyCors)
+  dagger2 <- crossprod(t(xRowSum * xRowSum2), Yvec)
+  
+  
+  
+  return((star1 + 0.25 * (star2 + star3 + star4) - dagger1 - dagger2) / (n - 1))
+  
+  
+}
+
+varcalc1_multi <- function (Ymat, Xmat) {
+  
+  Xmat <- scale(Xmat)
+  Ymat <- scale(Ymat)
+  
+  n <- nrow(Xmat)
+  
+  if (n != nrow(Ymat))
+    stop('X and Y variables must be of same dimension\n')
+  
+  # General calcs
+  xyCors <- cor(Ymat, Xmat)
+  y4 <- colSums(Ymat^4)
+  xRowSum <- rowSums(Xmat)
+  xRowSum2 <- tcrossprod(xyCors, Xmat^2)
+  
+  # Calc for star 1
+  star1 <- crossprod(Ymat^2, xRowSum^2)
+  
+  # Calc for star 2
+  star2 <- y4 * rowSums(xyCors)^2
+  
+  # Calc for star 3
+  star3 <- 2 * rowSums(xyCors) * colSums(Ymat^2 * t(xRowSum2))
+  
+  # Calc for star 4
+  star4 <- rowSums(xRowSum2^2)
+  
+  # Calc for dagger 1
+  dagger1 <- rowSums(xyCors) * crossprod(Ymat^3, xRowSum)
+  
+  # Calc for dagger 2
+  dagger2 <- colSums(xRowSum * t(xRowSum2) * Ymat)
   
   return((star1 + 0.25 * (star2 + star3 + star4) - dagger1 - dagger2) / (n - 1))
   
