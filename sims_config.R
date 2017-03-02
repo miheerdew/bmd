671e71f28330fb0d7057014425c0bca113a13fac
Miheer Dewaskar <miheerdew@gmail.com>
2017-03-02 18:02:17 -0500

    Fixed bug in run_script; moved RUN to sims_config

--- sims_config.R
+++ sims_config.R
@@ -8,6 +8,10 @@ simname <- "manyblocks-sparse"
 ns <- c(100, 500, seq(1000, 5000, by = 1000)) #The sample sizes to run.
 doBRIM <- FALSE #Should we test BRIM?
 
+RUN <- list(sims    =TRUE,
+            methods =TRUE,
+            plots   =TRUE)
+
 plot_full_mat <- FALSE
 
 saveDir <- file.path("sims", simname)
