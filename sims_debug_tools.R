e051701cee33388c7c979f9bc71d22f4b9c22855
Miheer Dewaskar <miheerdew@gmail.com>
2017-03-09 23:13:15 -0500

    p-value plotting function

--- sims_debug_tools.R
+++ sims_debug_tools.R
@@ -25,3 +25,64 @@ bmd_index <- function(u){
   #Otherwise it must be noise.
   return(0)
 }
+
+#The indices corresponding to the bimodules. Accessible as list
+bmd_indices <- c(lapply(1:nBM, function(b) 1:(nB*sB) + (b-1)*nB*sB))
+noise_indices <- 1:(nBg*sB) + signal_nodes
+
+plot_pvals_by_bmd <- function(pvals, alpha, ...){
+  old.par <- par(mfrow=c(2,2), mar=c(2,2,2,2))
+  for (i in 1:nBM){
+    plot_pvals(pvals[bmd_indices[[i]]], alpha,
+              bmd_name=paste("BMD",i), ...)
+  }
+  par(old.par)
+}
+
+plot_pvals <- function(pvals, alpha, bmd_name,
+                       transformed=FALSE, cap=Inf){
+  if(transformed){
+    transform <- function(x) { -log10(x) }
+    legend_loc <- "topright"
+    ylab <- "-log(p(i))"
+  } else {
+    transform <- identity
+    legend_loc <- "topleft"
+    ylab <- "p(i)"
+  }
+
+  n <- length(pvals)
+  pvals <- pmin(transform(sort(pvals)),cap)
+  thresh_line  <- transform(c(1:n) * alpha / n)
+  plot(pvals, xlab="i (p-val rank)", ylab=ylab, pch=1)
+  lines(1:n, thresh_line, col = "purple")
+  title(bmd_name)
+}
+
+plot_pval_simple <- function(pvals, alpha, group_index){
+  n <- length(pvals)
+  groups = sapply(order(pvals),group_index)
+  group_nums = sort(unique(groups))
+  plot(sort(pvals), col = 1 + groups)
+  lines(1:n, c(1:n) * alpha / n, col = "purple")
+  legend("topleft",
+         legend = c(paste0("group ", group_nums), "BH line"),
+         col = c(1 + group_nums, "purple"),
+         lty = c(rep(NA, length(group_nums)), 1),
+         pch = c(rep(1, length(group_nums)), NA)
+        )
+}
+
+plot_pval_transformed <- function(pvals, alpha, group_index){
+  n <- length(pvals)
+  groups = sapply(order(pvals),group_index)
+  group_nums = sort(unique(groups))
+  plot(-log10(sort(pvals)), col = 1 + groups)
+  lines(1:n, -log10(c(1:n) * alpha / n), col = "purple")
+  legend("topright",
+         legend = c(paste0("group ", group_nums), "BH line"),
+         col = c(1 + group_nums, "purple"),
+         lty = c(rep(NA, length(group_nums)), 1),
+         pch = c(rep(1, length(group_nums)), NA)
+        )
+}
