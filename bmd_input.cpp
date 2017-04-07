6ae332305cf0f9fcf7984414792991f5afcc5b20
Miheer Dewaskar <miheerdew@gmail.com>
2017-04-06 23:48:30 -0400

    lower memory profile

--- bmd_input.cpp
+++ bmd_input.cpp
@@ -47,10 +47,10 @@ private:
 
 public:
 
-  BmdInput(mat X_scaled, mat Y_scaled, double alpha_=0.05) :
+  BmdInput(const mat& X_scaled, const mat& Y_scaled, double alpha_=0.05) :
     X(X_scaled), Y(Y_scaled),
-    X2(pow(X,2)), X3(pow(X,3)), X4sum(sum(pow(X,4))),
-    Y2(pow(Y,2)), Y3(pow(Y,3)), Y4sum(sum(pow(Y,4))),
+    X2(pow(X,2)), X3(X2%X), X4sum(sum(pow(X2,2))),
+    Y2(pow(Y,2)), Y3(Y2%Y), Y4sum(sum(pow(Y2,2))),
     dx(X_scaled.n_cols),
     dy(Y_scaled.n_cols),
     n(X_scaled.n_rows),
@@ -108,7 +108,8 @@ public:
   }
 
 protected :
-  mat first, f2, f3, f4sum, second, cross;
+  const mat *first, *f2, *f3; const rowvec *f4sum;
+  mat cross, second;
   vec corsums;
 
   vec pvals(){
@@ -120,11 +121,11 @@ protected :
   void set_sides(uvec B, bool test_x){
     if (test_x){
       cross = cormat.cols(B);
-      first = X; f2 = X2; f3=X3; f4sum = X4sum;
+      first = &X; f2 = &X2; f3=&X3; f4sum = &X4sum;
       second = Y.cols(B);
     } else {
       cross = cormat.rows(B).t();
-      first = Y; f2 = Y2; f3=Y3; f4sum = Y4sum;
+      first = &Y; f2 = &Y2; f3=&Y3; f4sum = &Y4sum;
       second = X.cols(B);
     }
     corsums = sum(cross, 1);
@@ -133,13 +134,13 @@ protected :
   vec vars(){
     vec sRowSum = sum(second, 1); // n x 1
     mat sRowSum2 = cross * pow(second,2).t(); // p x n
-    vec star1 = f2.t() * pow(sRowSum,2); // p x 1
-    vec star2 = f4sum.t() % pow(corsums,2); //p x 1
-    vec star3 = 2 * corsums % sum(f2 % sRowSum2.t()).t(); // p x 1
+    vec star1 = f2->t() * pow(sRowSum,2); // p x 1
+    vec star2 = f4sum->t() % pow(corsums,2); //p x 1
+    vec star3 = 2 * corsums % sum((*f2) % sRowSum2.t()).t(); // p x 1
     vec star4 = sum(pow(sRowSum2,2),1); // p x 1
-    vec dagger1 = corsums % (f3.t() * sRowSum); // p x 1
+    vec dagger1 = corsums % (f3->t() * sRowSum); // p x 1
 
-    mat aux = first % sRowSum2.t(); // n x p
+    mat aux = (*first) % sRowSum2.t(); // n x p
     rowvec dagger2 = sum(aux.each_col() % sRowSum); // 1 x p
 
     return (star1 + 0.25 * (star2 + star3 + star4) - dagger1 - dagger2.t()) / (n - 1);
